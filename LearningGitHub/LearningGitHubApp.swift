729412da92c69d0b2650c856c8d4d7ec90220d56
Rune Pollet <runepollet@icloud.com>
2022-01-28 11:40:17 +0100

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Rune Pollet on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
